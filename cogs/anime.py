2569ce0a767001cc0c4ad1aa845eacb591c07c57
Dominik <01625726@student.tuwien.ac.at>
2020-03-07 02:56:52 +0100

    deleted unused code

--- cogs/anime.py
+++ cogs/anime.py
@@ -19,64 +19,6 @@ class Anime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # async def getVid(self, link):
-    #     if not link.startswith("https://aniwatcher.com") and not link.startswith("http://aniwatcher.com"):
-    #         link = WEBSITE + link
-    #     print(link)
-    #     session = AsyncHTMLSession()
-    #     try:
-    #         resp = await session.get(link)
-    #     except:
-    #         return "error"
-    #     try:
-    #         await resp.html.arender(timeout=10,wait=5)
-    #     except:
-    #         return "timeout : " + link
-    #     dt = resp.html.find(".srcnopop", first=True)
-    #     if (dt == None):
-    #         dt = resp.html.find(".srcpop", first=True)
-    #     if (dt == None):
-    #         x = "error"
-    #     else:
-    #         x = dt.attrs['src']
-    #         if not x.startswith("http"):
-    #             x = "http:" + x
-    #     print("test ", link)
-    #     return x
-    #
-    # async def random(self, aid, ctx):
-    #     '''gets the video url from a random anime from aniwatcher.com'''
-    #     data = requests.get("https://aniwatcher.com/random.php")
-    #     url = data.url
-    #     page = 1
-    #     sel = CSSSelector('a')
-    #     genre = "  "
-    #     while True:
-    #         lx = lxml.html.fromstring(data.content)
-    #         title = lx.findtext('.//title')
-    #         img = lx.find_class('imgseries')[0].find("img").get('src')
-    #         ep1 = lxml.html.fromstring(data.content).find_class("ep")
-    #         if len(ep1) <= 0:
-    #             print("error")
-    #         ep1 = WEBSITE + ep1[-1].get('href')
-    #         if "episode-1" in ep1:
-    #             for g in sel(lx.get_element_by_id('series')):
-    #                 genre += "{}, ".format(g.text_content())
-    #                 genre = genre[:-2]
-    #             break
-    #         page += 1
-    #         data = requests.get(url + "?page=" + str(page))
-    #     embed = discord.Embed(title=title, description=data.url, color=0xeee657)
-    #     embed.add_field(name="ID", value=aid, inline=True)
-    #     embed.add_field(name="Votes", value="0", inline=True)
-    #     embed.add_field(name="Genres", value=genre, inline=True)
-    #     embed.set_thumbnail(url=WEBSITE + img) #set_image
-    #     msg = await ctx.send(embed=embed)
-    #     data2 = requests.get(ep1)
-    #     sources = sel(lxml.html.fromstring(data2.content).get_element_by_id("sources"))
-    #     self.bot.var[ctx.message.guild.id]['anime_msg'][aid] = msg
-    #     self.bot.var[ctx.message.guild.id]['anime'][aid] = sources
-
     async def random(self, aid, ctx):
         l = "https://www16.gogoanime.io/anime-list.html?page={}".format(random.randint(0, 54))
         data = requests.get(l)
@@ -98,8 +40,6 @@ class Anime(commands.Cog):
         l = "https://www16.gogoanime.io" + vid
         data = requests.get(l)
         sources = sel(lxml.html.fromstring(data.content).find_class("anime_muti_link")[0])
-        # for a in vids:
-        #     print(a.get("data-video"))
 
         embed = discord.Embed(title=title, description=url, color=0xeee657)
         embed.add_field(name="ID", value=aid, inline=True)
@@ -113,8 +53,6 @@ class Anime(commands.Cog):
         self.bot.var[ctx.message.guild.id]['anime'][aid] = sources
 
     def getvid(self, link):
-        # if not link.startswith("https://www16.gogoanime.io/") and not link.startswith("http://www16.gogoanime.io/"):
-        #     link = "https://www16.gogoanime.io/" + link
         if not link.startswith("http"):
             link = "http:" + link
             return link
@@ -173,9 +111,7 @@ class Anime(commands.Cog):
             aid = random.choice(list(self.bot.var[ctx.message.guild.id]['anime'].keys()))
         embed = self.bot.var[ctx.message.guild.id]['anime_msg'][aid].embeds[0]
         embed.clear_fields()
-        # result = await asyncio.gather(*[self.getVid(l.get('href')) for l in self.bot.var[ctx.message.guild.id]['anime'][aid]])
-        # for it, r in enumerate(result):
-        #     embed.add_field(name="stream {}".format(it + 1), value=r, inline=False)
+
         sources = self.bot.var[ctx.message.guild.id]['anime'][aid]
         for it, r in enumerate(sources):
             embed.add_field(name="stream {}".format(it + 1), value=self.getvid(r.get("data-video")), inline=False)
